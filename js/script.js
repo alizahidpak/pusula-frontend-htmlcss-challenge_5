adf25c8d64b4c080a7d26fb3dca97871c667030e
Ali Zahid Pak <alizahidpak@gmail.com>
2023-06-05 15:09:08 +0300

    First commit
    Queries added,
    Buttons working,
    Sticky navbar done

---
+++ js/script.js
@@ -0,0 +1,104 @@
+// ///////////////////////////////////////////////////////////
+// // Sticky navigation
+
+const sectionHeroEl = document.querySelector('.section-hero');
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add('sticky');
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove('sticky');
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: '-112px',
+  }
+);
+obs.observe(sectionHeroEl);
+
+// ///////////////////////////////////////////////////////////
+// // Make mobile navigation work
+
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+
+btnNavEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
+});
+
+///////////////////////////////////////////////////////////
+// FAQ Buttons
+
+const btnFaqEl = document.getElementsByClassName('collapse-expand');
+
+for (let i = 0; i < btnFaqEl.length; i++) {
+  btnFaqEl[i].addEventListener('click', function () {
+    const icon = this.querySelector('i');
+    icon.classList.toggle('icon-collapse');
+    this.classList.toggle('expanded');
+    const answer = this.nextElementSibling;
+    if (answer.style.maxHeight) {
+      answer.style.maxHeight = null;
+    } else {
+      answer.style.maxHeight = answer.scrollHeight + 'px';
+    }
+  });
+}
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll('a:link');
+
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    // Scroll back to top
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+
+    // Scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open');
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+// function checkFlexGap() {
+//   var flex = document.createElement('div');
+//   flex.style.display = 'flex';
+//   flex.style.flexDirection = 'column';
+//   flex.style.rowGap = '1px';
+
+//   flex.appendChild(document.createElement('div'));
+//   flex.appendChild(document.createElement('div'));
+
+//   document.body.appendChild(flex);
+//   var isSupported = flex.scrollHeight === 1;
+//   flex.parentNode.removeChild(flex);
+//   console.log(isSupported);
+
+//   if (!isSupported) document.body.classList.add('no-flexbox-gap');
+// }
+// checkFlexGap();
